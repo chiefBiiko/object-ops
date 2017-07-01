b9627f32f7759482e518f93d821b34b8c671721c
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-07-01 13:00:19 +0200

    pieces

--- cipher.js
+++ cipher.js
@@ -1,18 +1,17 @@
 const crypto = require('crypto')
 
-const cipher = {
-  encrypt: (plaintext, key) => {
-    const cipher = crypto.createCipher('aes-256-cbc', key)
-    var crypt = cipher.update(plaintext, 'utf8', 'base64')
-    crypt += cipher.final('base64')
-    return crypt
-  },
-  decrypt: (ciphertext, key) => {
-    const decipher = crypto.createDecipher('aes-256-cbc', key)
-    var drypt = decipher.update(ciphertext, 'base64', 'utf8')
-    drypt += decipher.final('utf8')
-    return drypt
-  }
+const encrypt = (plaintext, key) => {
+  const cipher = crypto.createCipher('aes-256-cbc', key)
+  var crypt = cipher.update(plaintext, 'utf8', 'base64')
+  crypt += cipher.final('base64')
+  return crypt
+}
+  
+const decrypt = (ciphertext, key) => {
+  const decipher = crypto.createDecipher('aes-256-cbc', key)
+  var drypt = decipher.update(ciphertext, 'base64', 'utf8')
+  drypt += decipher.final('utf8')
+  return drypt
 }
 
-module.exports = cipher
+module.exports = {encrypt: encrypt, decrypt: decrypt}
